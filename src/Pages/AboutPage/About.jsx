cb1d67de78464a2aa5bbf498bb96aca5196248d3
hackwellbn <oburasusan1@gmail.com>
2024-06-01 02:09:25 +0300

    pending updates

--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import {AboutFeatures} from '../../assets/assets'
+import {AboutFeatures} from '../../assets/assets'
 import './About.css'
 const About = () => {
     return (
         <div className='about'>
             <div className="about-hero">
                 <div className="image">
-                    {/* <img src={AboutFeatures} alt="" /> */}
-                    {/* <p>{AboutFeatures.title}</p> */}
+                     <p>{AboutFeatures[0].title}</p>
                 </div>
             </div>
         </div>
