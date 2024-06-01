dd574aeabbde6e4475fa1434e17642cd9118e98e
hackwellbn <oburasusan1@gmail.com>
2024-06-01 16:46:16 +0300

    updating

--- src/Pages/HomePage/layouts/About/About.jsx
+++ src/Pages/HomePage/layouts/About/About.jsx
@@ -18,7 +18,6 @@ const About = () => {
                                 <div className="about-description about-flex">
                                     <h3>{item.heading}</h3>
                                     <p>{item.description}</p>
-                                    {/* Add a Link to the Mission page */}
                                     <Link to="/Mission" className="btn">Mission</Link>
                                     {item.buttonText && item.buttonLink && (
                                         <div className="btns">
