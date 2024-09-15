662e7cfe4b9189e5255d24b51c12f92dc763ac9f
hackwellbn <oburasusan1@gmail.com>
2024-09-15 23:29:43 +0300

    pending updates

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -30,7 +30,7 @@ import structure_2 from "./sk_2.jpg"
 import metals from './metals.jpg'
 import metals_2 from './metals2.jpg'
 import metals_3 from './metals3.jpg'
-import Jush from "./romans6_3.jpeg"
+import Jush from "./FB_IMG_1726146459038.jpg"
 import Videoweld2 from './videoweld2.mp4'
 // import consultation_2 from './consultation2.jpg'
 import Designingplanning from "./bluePrint.jpg";
